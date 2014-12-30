74c78c4bb48825b9d148e51278c36d1606c0b868
nickcluc <nclucas01@gmail.com>
2014-12-30 13:52:39 -0500

    Fixed Hound Errors AGAIN FOR THE 90TH TIME

--- spec/features/user_views_account_details_spec.rb
+++ spec/features/user_views_account_details_spec.rb
@@ -17,7 +17,6 @@ feature "user sees account details", %Q{
 
     visit user_path(user)
 
-
     expect(page).to have_content user.first_name
     expect(page).to have_content user.last_name
     expect(page).to have_content user.email
