a9b634ff84d4d6ea1d89d4381465e994e4a2d1cc
Jenny Brown <jennyzbrown@gmail.com>
2014-12-31 15:57:35 -0500

    added tests waiting for other pair

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :sites
+  validates :role, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
