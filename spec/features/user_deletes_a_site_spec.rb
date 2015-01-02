a9536656801fa348cfbe5e44fe1ca76d797214bc
nickcluc <nclucas01@gmail.com>
2015-01-02 16:16:13 -0500

    Added delete site function

---
+++ spec/features/user_deletes_a_site_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+feature "User Deletes A Site", %{
+  As a user,
+  I want to delete a site that I created,
+  So that I can remove old sites.
+
+  Acceptance Criteria:
+
+  [ ] If i don't own the site, I should not see a delete button on the site page
+  [ ] If I own the site, i should see a delete button on the site page
+} do
+
+  let(:site) do
+    FactoryGirl.create(:site)
+  end
+
+  scenario "logged in user deletes a site they created" do
+    sign_in_as(site.user)
+
+    visit site_path(site)
+
+    click_on "Delete Site"
+
+    expect(current_path) == sites_path
+
+  end
+
+  scenario "non logged in user should not see delete button" do
+    visit site_path(site)
+
+    expect(page).to_not have_content(:link_or_button, "Delete Site")
+  end
+
+end
