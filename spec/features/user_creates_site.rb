6ce16dc07e5635dbb99bfcbfd457e3a029ace79f
Kiran Sridhar <kiran7773@hotmail.com>
2015-01-02 09:09:44 -0500

    fixed errors with show page
    entire test package running now

--- spec/features/user_creates_site.rb
+++ spec/features/user_creates_site.rb
@@ -5,44 +5,11 @@ feature "user registers", %{
   I want to be able to create a site to work
   So that I can share it with others
   } do
-  #
-  # Acceptance Criteria:
-  # -Must give name location, address, category of place, picture(optional), amenities
-  # -should be brought to the ddetails page for the meetup after I create it.
-  # -have to be signed in, in order to create it
-  # -should be able to create the location from the index page
-  # -should see a message that lets me know that I have created a meetup successfully
-  # -should see error if not created successfully
-  # -I should be redirected to a log in page if i try to go to the new site page while logged out
-
-
-  # before :each do
-  #   # user = FactoryGirl.create(:user)
-  #
-  #   # visit new_user_registration_path
-  #   # fill_in "Email", with: user.email
-  #   # fill_in "Password", with: user.password
-  #   # fill_in "First", with: user.first_name
-  #   # fill_in "Last", with: user.last_name
-  #   # fill_in "Password confirmation", with: user.password
-  #   #
-  #   # click_button "Sign up"
-  #
-  #   visit new_user_session_path
-  #
-  #   fill_in "Email", with: user.email
-  #   fill_in "Password", with: user.password
-  #
-  #   click_button "Sign in"
-  #
-  # end
 
   scenario "if signed in" do
 
-
       site = FactoryGirl.create(:site)
       user = User.find(site.user_id)
-
 
       visit new_user_session_path
 
@@ -52,7 +19,6 @@ feature "user registers", %{
       click_button "Sign in"
 
       visit new_site_path
-
 
       fill_in "Name", with: site.name
       fill_in "Address", with: site.address
@@ -64,8 +30,6 @@ feature "user registers", %{
       fill_in "URL", with: site.url
       fill_in "Cost Rating", with: site.cost_rating
       click_on "Create Site"
-
-
 
       expect(page).to have_content site.name
       expect(page).to have_content site.address
@@ -79,11 +43,10 @@ feature "user registers", %{
       expect(page).to have_content "Edit"
       expect(page).to have_content "Site created successfully"
 
-    end
+  end
   scenario "site is not created successfully" do
     site = FactoryGirl.create(:site)
     user = User.find(site.user_id)
-
 
     visit new_user_session_path
 
@@ -113,7 +76,4 @@ feature "user registers", %{
 
     expect(page).to have_content "Create new site"
   end
-
-
-
 end
