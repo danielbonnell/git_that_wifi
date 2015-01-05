b4885e4d6f49ac5bf810961836f8920b5edbfa61
nickcluc <nclucas01@gmail.com>
2015-01-05 17:15:37 -0500

    fixed hound error

--- db/migrate/20150105195908_add_column_user_id_to_reviews.rb
+++ db/migrate/20150105195908_add_column_user_id_to_reviews.rb
@@ -1,5 +1,5 @@
 class AddColumnUserIdToReviews < ActiveRecord::Migration
   def change
-    add_column :reviews, :user_id, :integer, null:false
+    add_column :reviews, :user_id, :integer, null: false
   end
 end
