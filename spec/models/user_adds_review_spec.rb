231522f1d9b061c17e0dd7bc66cac51f53ca619a
Kiran Sridhar <kiran7773@hotmail.com>
2015-01-08 13:34:09 -0500

    Fixed Hound Error

--- spec/models/user_adds_review_spec.rb
+++ spec/models/user_adds_review_spec.rb
@@ -1,20 +1,20 @@
 require "rails_helper"
 RSpec.describe ReviewAdded, :type => :model do
 
-    it "posts a valid review" do
-      review = FactoryGirl.build(:review)
-      review.should be_valid
-    end
+  it "posts a valid review" do
+    review = FactoryGirl.build(:review)
+    review.should be_valid
+  end
 
-    it "receives an email" do
-      ActionMailer::Base.deliveries = []
-      prev_mail_count = ActionMailer::Base.deliveries.count
-      FactoryGirl.build(:site).reviewed
-      expect(ActionMailer::Base.deliveries.count).to eql(prev_mail_count + 1)
-    end
+  it "receives an email" do
+    ActionMailer::Base.deliveries = []
+    prev_mail_count = ActionMailer::Base.deliveries.count
+    FactoryGirl.build(:site).reviewed
+    expect(ActionMailer::Base.deliveries.count).to eql(prev_mail_count + 1)
+  end
 
-    it "posts an invalid review" do
-      review = FactoryGirl.build(:review, rating: nil)
-      review.should_not be_valid
-    end
+  it "posts an invalid review" do
+    review = FactoryGirl.build(:review, rating: nil)
+    review.should_not be_valid
+  end
 end
