e026f97c815f9cce3db99150133994967a06a812
Kiran Sridhar <kiran7773@hotmail.com>
2015-01-08 13:24:25 -0500

    Fixed to match Dan's suggestions

--- spec/models/user_adds_review_spec.rb
+++ spec/models/user_adds_review_spec.rb
@@ -1,8 +1,7 @@
 require "rails_helper"
 RSpec.describe ReviewAdded, :type => :model do
 
-  describe "valid review" do
-    it 'posts a valid' do
+    it "posts a valid review" do
       review = FactoryGirl.build(:review)
       review.should be_valid
     end
@@ -13,12 +12,9 @@ RSpec.describe ReviewAdded, :type => :model do
       FactoryGirl.build(:site).reviewed
       expect(ActionMailer::Base.deliveries.count).to eql(prev_mail_count + 1)
     end
-  end
 
-  describe "invalid review" do
-    it 'posts an invalid review' do
+    it "posts an invalid review" do
       review = FactoryGirl.build(:review, rating: nil)
       review.should_not be_valid
     end
-  end
 end
