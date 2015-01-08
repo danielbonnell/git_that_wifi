1a902a337d6153cd7b7ac18f49d3a758b07d8e82
chanhpark <chanhyun.park1211@gmail.com>
2015-01-08 16:18:20 -0500

    fixed hound errors

--- spec/features/search_sites_spec.rb
+++ spec/features/search_sites_spec.rb
@@ -4,7 +4,7 @@ feature "search movies" do
   scenario "fill in search form and view results" do
     cafe = FactoryGirl.create(:site, name: "Cafe Nero")
     cafe2 = FactoryGirl.create(:site, name: "Cafe Starbucks")
-    cafe3 = FactoryGirl.create(:site, name: "Dunkin")
+    FactoryGirl.create(:site, name: "Dunkin")
 
     visit sites_path
     fill_in "query", with: "Cafe"
@@ -12,6 +12,6 @@ feature "search movies" do
 
     expect(page).to have_link("Cafe Nero", href: site_path(cafe))
     expect(page).to have_link("Cafe Starbucks", href: site_path(cafe2))
-    expect(page).to_not have_content(cafe3.name)
+    expect(page).to_not have_content("Dunkin")
   end
 end
