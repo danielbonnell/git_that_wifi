ec69978b6f09a343e27d5881f2b2f3459ac2a2f6
nickcluc <nclucas01@gmail.com>
2015-01-09 15:15:52 -0500

    Fix hound errors

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -1,8 +1,10 @@
 module AvatarHelper
   def avatar(img_size)
     if current_user.avatar == nil
-      image_tag current_user.gravatar_url(size: img_size),
+      image_tag current_user.gravatar_url(
+        size: img_size,
         alt: "#{current_user.first_name} #{current_user.last_name}'s Avatar"
+      )
     else
       image_tag(
         current_user.avatar,
