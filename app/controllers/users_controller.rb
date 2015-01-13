90352d6256dbbc70631cb789ca06ff43d2b843e7
nickcluc <nclucas01@gmail.com>
2015-01-13 16:34:33 -0500

    changed size of search box

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @sites = @user.sites
-    @reviews =@user.reviews
+    @reviews = @user.reviews
   end
 end
